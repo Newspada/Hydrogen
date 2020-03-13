99707a3ce73e52b3e14f7695d8786a9d1ac08772
= <=>
2020-03-13 12:16:26 +0100

    Replaced Cartesian coordinates with cylindrical ones

--- HydrogenIon_Variational.py
+++ HydrogenIon_Variational.py
@@ -7,18 +7,17 @@ import matplotlib.pyplot as plt
 
 R=1                    
 x= sym.Symbol('x')
-y= sym.Symbol('y')
-z= sym.Symbol('z')
-ψa_sym=sym.exp(-sym.sqrt(x**2+y**2+z**2))/sym.sqrt(sym.pi)
-ψb_sym=sym.exp(-sym.sqrt((x-R)**2+y**2+z**2))/sym.sqrt(sym.pi)
+r= sym.Symbol('r')
+ψa_sym=sym.exp(-sym.sqrt(x**2+r**2))/sym.sqrt(sym.pi)
+ψb_sym=sym.exp(-sym.sqrt((x-R)**2+r**2))/sym.sqrt(sym.pi)
 
 def Laplacian(f):
-    return sym.diff(sym.diff(f, x), x)+sym.diff(sym.diff(f, y), y)+sym.diff(sym.diff(f, z), z)
+    return sym.diff(sym.diff(f, x), x)
 
-ψa=sym.lambdify([x, y, z], ψa_sym, "numpy")
-ψb=sym.lambdify([x, y, z], ψb_sym, "numpy")
-Δψa=sym.lambdify([x, y, z], Laplacian(ψa_sym), "numpy")
-Δψb=sym.lambdify([x, y, z], Laplacian(ψb_sym), "numpy")
+ψa=sym.lambdify([x, r], ψa_sym, "numpy")
+ψb=sym.lambdify([x, r], ψb_sym, "numpy")
+Δψa=sym.lambdify([x, r], Laplacian(ψa_sym), "numpy")
+Δψb=sym.lambdify([x, r, Laplacian(ψb_sym), "numpy")
 
 H=np.zeros((2, 2))
 S=np.zeros((2, 2))
