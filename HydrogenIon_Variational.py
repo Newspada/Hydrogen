790115aa2e5c556853e1936f8c7b595571227413
Leonardo Facchini <leonardo.facchini@studio.unibo.it>
2020-03-13 14:34:43 +0100

    Replaced greek letters

--- HydrogenIon_Variational.py
+++ HydrogenIon_Variational.py
@@ -1,21 +1,21 @@
 import numpy as np
+from numpy import pi
 import scipy as sp
 import sympy as sym
 from scipy import integrate
 from scipy import linalg
 import matplotlib.pyplot as plt
 
-R=1                    
+R=1                  
 x= sym.Symbol('x')
 r= sym.Symbol('r')
-ψa_sym=sym.exp(-sym.sqrt(x**2+r**2))/sym.sqrt(sym.pi)
-ψb_sym=sym.exp(-sym.sqrt((x-R)**2+r**2))/sym.sqrt(sym.pi)
+ψ_sym=(sym.exp(-sym.sqrt((x+R/2)**2+r**2))/sym.sqrt(sym.pi), sym.exp(-sym.sqrt((x-R/2)**2+r**2))/sym.sqrt(sym.pi))
 
 def Laplacian(f):
     return sym.diff(sym.diff(f, x), x) + sym.diff(r*sym.diff(f, r), r)/r
 
-ψ=(sym.lambdify([x, r], ψa_sym, "numpy"), sym.lambdify([x, r], ψb_sym, "numpy"))
-Δψ=(sym.lambdify([x, r], Laplacian(ψa_sym), "numpy"), sym.lambdify([x, r], Laplacian(ψb_sym), "numpy"))
+ψ=(sym.lambdify([x, r], ψ_sym[0], "numpy"), sym.lambdify([x, r], ψ_sym[1], "numpy"))
+laplψ=(sym.lambdify([x, r], Laplacian(ψ_sym[0]), "numpy"), sym.lambdify([x, r], Laplacian(ψ_sym[1]), "numpy"))
 
 H=np.zeros((2, 2))
 S=np.zeros((2, 2))
@@ -23,10 +23,11 @@ S=np.zeros((2, 2))
 for i in range(2):
     for j in range(2):
         def H_integrand(x, r):
-            return 2*np.pi*r*ψ[i](x, r)*(-0.5*Δψ[j](x, r)-((x**2+r**2)**(-0.5)-((x-R)**2+r**2)**(-0.5))*ψ[j](x, r))
+            return 2*pi*r*ψ[i](x, r)*(-0.5*laplψ[j](x, r)-(((x+R/2)**2+r**2)**(-0.5)+((x-R/2)**2+r**2)**(-0.5))*ψ[j](x, r))
         def S_integrand(x, r):
-            return 2*np.pi*r*ψ[i](x, r)*ψ[j](x, r)
-        H[i][j], err=sp.integrate.nquad(H_integrand, [[-10, +10], [0, +10]])
-        S[i][j], err=sp.integrate.nquad(S_integrand, [[-10, +10], [0, +10]])
-print(H, S)
+            return 2*pi*r*ψ[i](x, r)*ψ[j](x, r)
+        H[i][j], err=sp.integrate.nquad(H_integrand, [[-np.inf, +np.inf], [0, +np.inf]])
+        S[i][j], err=sp.integrate.nquad(S_integrand, [[-np.inf, +np.inf], [0, +np.inf]])
+print(H)
+print(S)
         
