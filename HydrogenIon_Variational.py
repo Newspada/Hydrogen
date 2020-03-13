9ee8d730019f464fee1ec1712a632b7ee49f0a1f
= <=>
2020-03-13 13:37:48 +0100

    Tried to complete the definitions of matrix elements H and S, but some errors occurred...

--- HydrogenIon_Variational.py
+++ HydrogenIon_Variational.py
@@ -14,10 +14,19 @@ r= sym.Symbol('r')
 def Laplacian(f):
     return sym.diff(sym.diff(f, x), x) + sym.diff(r*sym.diff(f, r), r)/r
 
-ψa=sym.lambdify([x, r], ψa_sym, "numpy")
-ψb=sym.lambdify([x, r], ψb_sym, "numpy")
-Δψa=sym.lambdify([x, r], Laplacian(ψa_sym), "numpy")
-Δψb=sym.lambdify([x, r], Laplacian(ψb_sym), "numpy")
+ψ=(sym.lambdify([x, r], ψa_sym, "numpy"), sym.lambdify([x, r], ψb_sym, "numpy"))
+Δψ=(sym.lambdify([x, r], Laplacian(ψa_sym), "numpy"), sym.lambdify([x, r], Laplacian(ψb_sym), "numpy"))
 
 H=np.zeros((2, 2))
 S=np.zeros((2, 2))
+
+for i in range(2):
+    for j in range(2):
+        def H_integrand(x, r):
+            return 2*np.pi*r*ψ[i](x, r)*(-0.5*Δψ[j](x, r)-((x**2+r**2)**(-0.5)-((x-R)**2+r**2)**(-0.5))*ψ[j](x, r))
+        def S_integrand(x, r):
+            return 2*np.pi*r*ψ[i](x, r)*ψ[j](x, r)
+        H[i][j], err=sp.integrate.nquad(H_integrand, [[-10, +10], [0, +10]])
+        S[i][j], err=sp.integrate.nquad(S_integrand, [[-10, +10], [0, +10]])
+print(H, S)
+        
